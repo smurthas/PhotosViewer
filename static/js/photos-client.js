4b57a9b46a5167cde7f914e989f73407256e5617
Simon Murtha-Smith <simon@murtha-smith.com>
2011-11-13 18:43:35 -0800

    updated to add a name dropdown selector to filter by person

--- static/js/photos-client.js
+++ static/js/photos-client.js
@@ -1,6 +1,8 @@
 /* Generic log function for debugging. */
 var log = function(msg) { if (console && console.log) console.debug(msg); };
+var sort = '\'{"timestamp":-1}\'';
 var baseUrl = '';
+var placeholder = 'Everyone';
 
 function photoApp() {
     // set the params if not specified
@@ -8,100 +10,120 @@ function photoApp() {
     var limit = 50;
     var $photosList = $("#main");
 
-    var timeSort = function(lh, rh) {
-                var rhc = parseInt(rh.timestamp);
-                var lhc = parseInt(lh.timestamp);
-                //log(rhc);
-                //log(lhc);
-                if (isNaN(rhc) || isNaN(lhc)) {
-                    console.dir(rh);
-                    console.dir(lh);
-                }
-                if (rhc > (Date.now() / 1000)) rhc = rhc / 1000;
-                if (lhc > (Date.now() / 1000)) lhc = lhc / 1000;
-                return rhc - lhc;
-    };
-
     var HTMLFromPhotoJSON = function(photos) {
-        var p, title, photoHTML = "";
-        // removed for the time being, the query should return time sorted data
-        // photos = photos.sort(timeSort);
+        var p,
+        title,
+        photoHTML = "";
         for (var i in photos) {
-      p = photos[i];
-      title = p.title ? p.title : "Unititled";
-      photoHTML += '<div class="box"><div id="' + p._id + '" class="photo"><img src="' + baseUrl + '/Me/photos/image/' + p.id+ '" style="max-width:300px" /><div class="basic-data">'+title+'</div></div></div>';
-  }
+            p = photos[i];
+            title = p.title ? p.title: "Untitled";
+            photoHTML += '<div class="box"><div id="' + p._id + '" class="photo"><img src="' + baseUrl + '/Me/photos/image/' + p.id + '" style="max-width:300px" /><div class="basic-data">' + title + '</div></div></div>';
+        }
         return photoHTML;
     };
 
     var getPhotosCB = function(photos) {
-        var p, title, photoHTML = "";
+        var p,
+        title,
+        photoHTML = "";
 
-  // clear the list
-  $photosList.html('');
+        // clear the list
+        $photosList.html('');
+        $photosList.masonry('destroy');
+        // populate the list with our photos
+        if (photos.length == 0) return $photosList.append("<div>Sorry, no photos found!</div>");
 
-  // populate the list with our photos
-  if (photos.length == 0) $photosList.append("<div>Sorry, no photos found!</div>");
+        var $newElems = $(HTMLFromPhotoJSON(photos));
+        $photosList.append($newElems);
 
-  $photosList.append(HTMLFromPhotoJSON(photos));
-
-        $photosList.imagesLoaded(
-            function(){
-                $photosList.masonry(
-                    {
-                        itemSelector : '.photo'
-                    });
-            });
+        // ensure that images load before adding to masonry layout
+        $newElems.imagesLoaded(function() {
+            $photosList.masonry({itemSelector: '.photo'});
+        });
         offset += photos.length;
     };
 
     var getMorePhotosCB = function(photos) {
-        var $newElems;
         if (photos.length == 0) return;
 
-  $newElems = $(HTMLFromPhotoJSON(photos));
+        var $newElems = $(HTMLFromPhotoJSON(photos));
         $photosList.append($newElems);
 
         // ensure that images load before adding to masonry layout
-        $newElems.imagesLoaded(
-            function(){
-                $photosList.masonry( 'appended', $newElems, true );
-            });
+        $newElems.imagesLoaded(function() {
+            $photosList.masonry('appended', $newElems, true);
+        });
         offset += limit;
     };
 
-    var sort = '\'{"timestamp":-1}\'';
 
-    var loadMorePhotosHandler = function() {
-        $.getJSON( baseUrl +
-      '/query/getPhoto',
-      {
-                'offset':offset,
-                'limit':limit,
-                'sort':sort
-            },
-            getMorePhotosCB
-        );
+    function loadPhotos(callback) {
+        var name = $("#names option:selected").val();
+        var terms = '';
+        if(name && name !== placeholder)
+            terms += "?terms=[sources.data.tags.data.name:'" + name + "']";
+        $.getJSON(baseUrl +'/query/getPhoto' + terms, 
+                  {offset: offset, limit: limit, sort: sort}, 
+                  callback);
+    }
 
-    };
-
-    // init
-    $.getJSON( baseUrl +
-  '/query/getPhoto',
-  {
-            'offset':offset,
-            'limit':limit,
-            'sort':sort
-    },
-  getPhotosCB
-    );
-
-    $("#moarphotos").click( loadMorePhotosHandler );
-
+    
+    getNames(function(names) {
+        populateNamesSelect(names);
+        loadPhotos(getPhotosCB);
+    });
+    
+    $("#moarphotos").click(function() {
+        loadPhotos(getMorePhotosCB);
+    });
+    $("#names").change(function() {
+        offset = 0;
+        // clear the list
+        $photosList.html('');
+        console.log('change!');
+        loadPhotos(getPhotosCB);
+    })
     // TODO: make this keep in sync!
+}
+
+function getNames(callback) {
+    $.getJSON(baseUrl + '/query/getPhoto?fields=[sources.data.tags.data.name:1]',{}, function(namesObjects) {
+        var namesHash = {};
+        for(var i in namesObjects) {
+            if(namesObjects[i].sources && namesObjects[i].sources[0] && namesObjects[i].sources[0].data && namesObjects[i].sources[0].data.tags) {
+                var data = namesObjects[i].sources[0].data.tags.data;
+                for(var j in data)
+                    namesHash[data[j].name] = 1;
+            }
+        }
+    // $.getJSON(baseUrl + '/query/getContact?fields=[name:1]',{}, function(namesObjects) {
+        var names = [];
+        
+        for(var i in namesHash) {
+            names.push(i);
+        }
+        // for(var i in namesObjects) {
+        //     var name =  namesObjects[i].name;
+        //     if(name && name !== undefined)
+        //         names.push(name);
+        // }
+        names.sort();
+        names.reverse();
+        names.push(placeholder);
+        names.reverse();
+        callback(names);
+    });
+}
+
+function populateNamesSelect(names) {
+    var namesSelect = $("#names");
+    namesSelect.html('');
+    for(var i in names) {
+        namesSelect.append('<option value="' + names[i] + '">' + names[i] + '</option>');
+    }
 }
 
 /* jQuery syntactic sugar for onDomReady */
 $(function() {
-      var photos = photoApp();
+    var photos = photoApp();
 });
